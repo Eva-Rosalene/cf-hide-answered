effd33a90855a8a187b0a8c52ac8062ff8d41bc8
Lera Rosalene <lerarosalene@outlook.com>
2020-07-04 10:08:40 +0300

    save state to localStorage

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,30 @@ import { UI } from "./UI";
 import { createElement } from "./factory";
 import styleSheet from "./style.css";
 
+function getInitialState() {
+  const savedState = localStorage.getItem('cf_hide_answered');
+  if (!savedState) {
+    return false;
+  }
+  try {
+    return !!JSON.parse(savedState);
+  } catch (error) {
+    return false;
+  }
+}
+
 const hider = new Hider();
 hider.init();
 
 const ui = new UI();
 ui.onUpdate = state => {
+  localStorage.setItem('cf_hide_answered', JSON.stringify(state));
   if (state) {
     hider.hide();
   } else {
     hider.reveal();
   }
 };
-ui.init();
 
+ui.init(getInitialState());
 document.head.appendChild(<style>{styleSheet}</style>);
